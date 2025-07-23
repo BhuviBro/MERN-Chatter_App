bc1d7f4b7f2b6aeca012b2b57877d1d1fd03e3c0
Bhuvan <bhuvibhuvan1624@gmail.com>
2025-07-23 16:06:10 +0530

    FE: Completed Till SideBar

---
+++ Client-FE/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import ProfilePage from './Pages/ProfilePage'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    
+  <div className=' bg-[url("./src/assets/bgImage.svg")] bg-contain bg-center bg-'>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+    </Routes>
+  </div>
+  )
+}
+
+export default App
